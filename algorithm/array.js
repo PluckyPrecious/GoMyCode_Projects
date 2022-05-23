e6be076d93540717b09f3406ae7d43e62c469149
PluckyPrecious <prekel27@gmail.com>
2022-05-23 11:29:29 +0100

    Update array.js
     The number of vowels in the sentence

--- algorithm/array.js
+++ algorithm/array.js
@@ -1,3 +1,18 @@
-function getCounts(string) {
-    
+//The length of the sentence (the number of characters).
+function getCounts(string) {} //
+
+
+// The number of vowels in the sentence
+function getVowels(string) {
+var Vowels = 'aAeEiIoOuU';
+var vowelsCount = 0;
+for(var i = 0; i < string.length ; i++) {
+ if (Vowels.indexOf(string[i]) !== -1) {
+    vowelsCount += 1;
 }
+}
+return vowelsCount;
+}
+document.write("The Number of vowels in -"+
+" Hello World! I am plucky and a proud fullstack Developer.:" 
++ getVowels("Hello World! I am Plucky, and a proud FullStack Developer."));
