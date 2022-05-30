e8f7a802e2186611f1e989c7469d777e1919c011
PluckyPrecious <prekel27@gmail.com>
2022-05-30 18:22:19 +0100

    add data structures

--- algorithm/algo.js
+++ algorithm/algo.js
@@ -1,30 +1,46 @@
-//The length of the sentence (the number of characters).
-function getCounts(string) {}
+// The length of the sentence (the number of characters)
+function getLength(string) {
+  var str = new String("Hello World! I am Plucky, and a proud FullStack Developer.");
+
+  return str.length;
+}
+document.write(
+  "The length of the sentence character -" +
+    "Hello World! I am Plucky, and a proud FullStack Developer.:" +
+    getLength("")
+);
+document.write("<br>");dc
 
 
-//The number of words in the sentence (assuming that the words are separated by a single space).
+// The number of words in the sentence (assuming that the words are separated by a single space)
 function countWords(str) {
-    str = str.replace(/(^\s*)|(\s*$)/gi,"");
-    str = str.replace(/[ ]{2,}/gi," ");
-    str = str.replace(/\n /,"\n");
-    return str.split(' ').length;
-    }
- document.write("The Number of words in the sentence -"+
- "Hello World! I am Plucky, and a proud FullStack Developer.:"
- + countWords("Hello World! I am Plucky, and a proud FullStack Developer."));
+  str = str.replace(/(^\s*)|(\s*$)/gi, "");
+  str = str.replace(/[ ]{2,}/gi, " ");
+  str = str.replace(/\n /, "\n");
+
+  return str.split(" ").length;
+}
+document.write(
+  "The Number of words in the sentence -" +
+    "Hello World! I am Plucky, and a proud FullStack Developer.:" +
+    countWords("Hello World! I am Plucky, and a proud FullStack Developer.")
+);
+document.write("<br>");
 
 
 // The number of vowels in the sentence
 function getVowels(string) {
-var Vowels = 'aAeEiIoOuU';
-var vowelsCount = 0;
-for(var i = 0; i < string.length ; i++) {
- if (Vowels.indexOf(string[i]) !== -1) {
-    vowelsCount += 1;
+  var Vowels = "aAeEiIoOuU";
+  var vowelsCount = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (Vowels.indexOf(string[i]) !== -1) {
+      vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
 }
-}
-return vowelsCount;
-}
-document.write("The Number of vowels in -"+
-" Hello World! I am Plucky, and a proud FullStack Developer.:" 
-+ getVowels("Hello World! I am Plucky, and a proud FullStack Developer."));
+document.write(
+  "The Number of vowels in -" +
+    " Hello World! I am Plucky, and a proud FullStack Developer.:" +
+    getVowels("Hello World! I am Plucky, and a proud FullStack Developer.")
+);
