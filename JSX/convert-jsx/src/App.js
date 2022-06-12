ee3976a9b861c1060f32bdf7aba80bc4b526b700
PluckyPrecious <prekel27@gmail.com>
2022-06-12 08:32:29 +0100

    Convert JSX
    Convert HTML code to JSX in App.js file

--- JSX/convert-jsx/src/App.js
+++ JSX/convert-jsx/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import "./style.css";
+import myVideo from "./myVideo.mp4";
+import imageSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw"}}>
+
+        <h1 class="title red">Kelechi Precious Nwachukwu </h1>
+
+        <br />
+
+        <img src={imageSrc} alt="imageInSrc" />
+
+        <br />
+
+        <img src="/imageInPublic.jpg" alt="imageInSrc" />
+
+      </div>
+
+      <video width="320" height="240" controls>
+
+        <source src={myVideo} type="video/mp4" />
+
+      </video>
     </div>
   );
 }
